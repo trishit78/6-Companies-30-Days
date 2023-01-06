0a6b6e1a641d60a33b33181e0d8699f571456ed0
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2023-01-06 23:11:14 +0530

    Day 4

---
+++ Goldman Sachs/Valid _Square.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+    int get_length(vector<int>& a, vector<int>& b){
+        return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]);
+    }
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        vector<int> sides = {
+            get_length(p1, p2),
+            get_length(p1, p3),
+            get_length(p1, p4),
+            get_length(p2, p3),
+            get_length(p2, p4),
+            get_length(p3, p4),
+        };
+        unordered_map<int, int> m;
+        for(auto& s: sides){
+            if(!m.count(s))
+                m.insert({s, 1});
+            else m[s]++;
+        }
+        
+        return !m.count(0) && m.size() == 2;
+        
+    }
+};
